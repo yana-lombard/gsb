afcedeab88e89b8e53a003ad02d34c593cba535e
Yana <youremail@yourdomain.com>
2023-10-17 15:24:41 +0200

    import donnée

--- src/Controller/DataImportController.php
+++ src/Controller/DataImportController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Entity\FicheFrais;
+use App\Entity\LigneFraisHorsForfait;
 use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -46,17 +50,41 @@ class DataImportController extends AbstractController
         ]);
     }
 
-    #[Route('/fiche-frais', name: 'app_data_import_ficheFrais')]
-    public function ficheFrais(EntityManagerInterface $entityManager): Response
+    #[Route('/fichefrais', name: 'app_data_import_ficheFrais')]
+    public function ficheFrais(EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response
     {
-        $usersjson = file_get_contents('./visiteur.json');
-        $users = json_decode($usersjson, true);
-        foreach ($users as $user) {
-            $newUser = new User();
-            $newUser->setOldId($user['id']);
+        $fichesFraisjson = file_get_contents('./ficheFrais.json');
+        $fichesFrais = json_decode($fichesFraisjson, true);
+
+        foreach ($fichesFrais as $ficheFrais) {
+            $newFf = new FicheFrais();
+            $newFf->setMois($ficheFrais['mois']);
+            $newFf->setNbJustificatifs($ficheFrais['nbJustificatifs']);
+            $newFf->setMontantValid($ficheFrais['montantValide']);
+            $newFf->setDateModif(new \DateTime($ficheFrais['dateModif']));
+            $user = $doctrine->getRepository(User::class)->findOneBy(['oldId' => $ficheFrais ['idVisiteur']]);
+            $newFf->setUser($user);
+            switch ($ficheFrais ['idEtat']) {
+                case "CL":
+                    $etat = $doctrine->getRepository(Etat::class)->find('1');
+                    break;
+                case "CR":
+                    $etat = $doctrine->getRepository(Etat::class)->find('2');
+                    break;
+                case "RB":
+                    $etat = $doctrine->getRepository(Etat::class)->find('3');
+                    break;
+                case "VA":
+                    $etat = $doctrine->getRepository(Etat::class)->find('4');
+                    break;
+                default:
+                    $etat = $doctrine->getRepository(Etat::class)->find('1');
+                    break;
+            }
+            $newFf->setEtat($etat);
 
 
-            $entityManager->persist($newUser);
+            $entityManager->persist($newFf);
 
         }
         $entityManager->flush();
@@ -64,4 +92,30 @@ class DataImportController extends AbstractController
             'controller_name' => 'DataImportController',
         ]);
     }
+
+    #[Route('/fichefraisHF', name: 'app_data_import_ficheFraisHF')]
+    public function ficheFraisHF(EntityManagerInterface $entityManager,  ManagerRegistry $doctrine): Response
+    {
+        $fichesFraisHFjson = file_get_contents('./lignefraishorsforfait.json');
+        $fichesFraisHF = json_decode($fichesFraisHFjson, true);
+
+        foreach ($fichesFraisHF as $ficheFraisHF) {
+            $newFfHF = new LigneFraisHorsForfait();
+            $newFfHF->setLibelle('libelle');
+            $newFfHF->setDate(new \DateTime($ficheFraisHF['date']));
+            $newFfHF->setMontant($ficheFraisHF['montant']);
+            $user = $doctrine->getRepository(User::class)->findOneBy(['oldId' => $ficheFraisHF['idVisiteur']]);
+            $ficheFrais = $doctrine->getRepository(FicheFrais::class)->findOneBy(['user' => $user, 'mois' => $ficheFraisHF['mois']]);
+            $newFfHF->setFicheFrais($ficheFrais);
+
+            $entityManager->persist($newFfHF);
+
+        }
+        $entityManager->flush();
+        return $this->render('data_import/index.html.twig', [
+            'controller_name' => 'DataImportController',
+        ]);
+    }
+
+
 }
